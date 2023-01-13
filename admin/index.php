a7b99c39b60494386ced928a065e201169710221
alisson2014 <almeidaalisson2014@gmail.com>
2023-01-13 20:06:09 -0300

    modified design admin

--- admin/index.php
+++ admin/index.php
@@ -8,16 +8,16 @@ include "../config.php";
 <html lang="pt-br">
 
 <head>
+  <!-- Tipografia: -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin do sistema</title>
   <!-- Folha de estilo: -->
   <link rel="stylesheet" href="style/container.css">
-  <!-- Tipografia: -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
 </head>
 
 <body>
