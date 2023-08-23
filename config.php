11974d235e87a4f62878c820b6bac5f84c708332
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-08-23 19:15:00 -0300

    ✨[BG-1] Alterei o arquivo config para trabalhar com PDO

--- config.php
+++ config.php
@@ -1,21 +1,17 @@
 <?php
-//criar uma conexao com banco de dados
-//endereco do servidor
-$servidor = "localhost";
-//usuario do banco
-$usuario = "root";
-//senha do banco
-$senha = "";
-//banco de dados a ser acessado
-$banco = "brogui";
+const SERVER = "localhost";
+const USER = "root";
+const PASSWORD = "";
+const DATA_BASE = "brogui";
+const CONFIG = "mysql:host=" . SERVER . ";dbname=" . DATA_BASE . ";charset=utf8;";
 
-$con = mysqli_connect($servidor, $usuario, $senha, $banco)
-    or die("Não foi possível conectar no banco");
+try {
+    $conn = new PDO(CONFIG, USER, PASSWORD);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    throw new PDOException($e->getMessage());
+}
 
-//força os dados a se tornarem utf8
-mysqli_set_charset($con, "utf8");
-
-//incluir uma função que mostra uma mensagem e retorna para tela anterior
 function mensagem($msg)
 {
     echo "<script>alert('{$msg}');history.back();</script>";
