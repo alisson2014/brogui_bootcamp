d12e739d22bde0395bf0062abb75162fe367d645
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-08-26 14:25:30 -0300

    ♻️ [BG-2] Melhoria/Refatorações  de código

--- admin/delete/noticia.php
+++ admin/delete/noticia.php
@@ -1,29 +1,31 @@
 <?php
+
 $id = $_GET["id"] ?? NULL;
 
 if (empty($id)) {
   mensagem("Registro inválido");
+}
+
+$id = (int)$id;
+$sql = "DELETE FROM noticia WHERE id = ?";
+
+$conn->beginTransaction();
+$stmt = $conn->prepare($sql);
+$stmt->bindValue(1, $id, PDO::PARAM_INT);
+$result = 0;
+
+try {
+  $stmt->execute();
+
+  $result = $stmt->rowCount();
+  $conn->commit();
+} catch (PDOException $e) {
+  $erro = $e->getMessage();
+  $conn->rollBack();
+}
+
+if ($result > 0) {
+  mensagem("Registro excluido");
 } else {
-  $id = (int)$id;
-  $sql = "DELETE FROM noticia WHERE id = ? LIMIT 1";
-
-  $conn->beginTransaction();
-  $stmt = $conn->prepare($sql);
-  $stmt->bindValue(1, $id, PDO::PARAM_INT);
-
-  try {
-    $stmt->execute();
-
-    $result = $stmt->rowCount();
-    $conn->commit();
-  } catch (PDOException $e) {
-    echo $e;
-    $conn->rollBack();
-  }
-
-  if ($result > 0) {
-    mensagem("Registro excluido");
-  } else {
-    mensagem("Erro ao excluir registro");
-  }
+  mensagem("Erro ao excluir registro.\n Erro: {$erro}");
 }
